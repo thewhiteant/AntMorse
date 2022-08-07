6206e5176d3a9cdeb6b0b3c8f7a5f4c730b30d45
White Ant <trafi227@gmail.com>
2022-08-07 14:06:48 +0600

    v1

--- Main.py
+++ Main.py
@@ -1,48 +1,80 @@
 from playsound import playsound
-from antapi import encrypt,decrypt
+from antapi import encrypt
   
-# for playing note.mp3 file
-def dot():
-    playsound('Dot.wav')
 
-def dash():
-     playsound('Dash.wav')
+def main():
 
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'," ":" ","!":"-.-.--",
-                    '"':".-..-.","#":"#","$":"$","%":"%","^":"^","&":".-...",
-                    "*":"*","=":"=","+":".-.-.","\ ":"\ ","/":"-..-.","<":"<",
-                    ">":">","~":"~",'\x7f':"\x7f","'":".----.","`":"`","|":"|"
-                    ,"//":"//","}":"}","{":"{","[":"]"
-                    }
+    def dot():
+        playsound('Dot.wav')
 
-text = input("A Text: ");
-text = encrypt(text)
-print(text)
-for i in text:
-    u = (MORSE_CODE_DICT[i.upper()])
-    print(u,end="")
-    for k in u:
-            if(k=="-"):
-                dash()
-            else:
-                dot()
+    def dash():
+        playsound('Dash.wav')
+
+    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                        'C':'-.-.', 'D':'-..', 'E':'.',
+                        'F':'..-.', 'G':'--.', 'H':'....',
+                        'I':'..', 'J':'.---', 'K':'-.-',
+                        'L':'.-..', 'M':'--', 'N':'-.',
+                        'O':'---', 'P':'.--.', 'Q':'--.-',
+                        'R':'.-.', 'S':'...', 'T':'-',
+                        'U':'..-', 'V':'...-', 'W':'.--',
+                        'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                        '1':'.----', '2':'..---', '3':'...--',
+                        '4':'....-', '5':'.....', '6':'-....',
+                        '7':'--...', '8':'---..', '9':'----.',
+                        '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                        '?':'..--..', '/':'-..-.', '-':'-....-',
+                        '(':'-.--.', ')':'-.--.-'," ":" ","!":"-.-.--",
+                        '"':".-..-.","#":"#","$":"$","%":"%","^":"^","&":".-...",
+                        "*":"*","=":"=","+":".-.-.","\ ":"\ ","/":"-..-.","<":"<",
+                        ">":">","~":"~",'\x7f':"\x7f","'":".----.","`":"`","|":"|"
+                        ,"//":"//","}":"}","{":"{","[":"]","\\":"\\"
+                        }
 
 
+    key_list = list(MORSE_CODE_DICT.keys())
+    val_list = list(MORSE_CODE_DICT.values())
+
+    def plaintexenmor():
+
+        text = input("A Text: ");
+        text = encrypt(text)
+        for i in text:
+            u = (MORSE_CODE_DICT[i.upper()])
+            print(u,end="")
+            for k in u:
+                    if(k=="-"):
+                        dash()
+                    else:
+                        dot()
+    
+    
 
 
 
+    def morseFile(name):
+       
+       f = open(name,"w")
+       text = input("A Text: ");
+       text = encrypt(text)
+       oo = ""
+       for i in text:
+            u = (MORSE_CODE_DICT[i.upper()])
+            oo = oo+u
+       f.write(oo)
+       f.close()
+
+
+
+    n = int(input("1.Encode\n2.Morse File\nChoose: "))
+    if(n==1):
+        plaintexenmor()
+    elif(n==2):
+        name = input("Enter File name: ")
+        morseFile(name)
+    else:
+        print("Wrong option")
+
+if __name__ == '__main__':
+    main()
 
